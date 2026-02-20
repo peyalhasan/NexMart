962a6e90810d6d6da6f9dd1dd75b75f2dd5be574
Peyal Hasan <peyalhasan143@gmail.com>
2026-02-20 15:28:54 +0600

    static params

--- app/(.)product/[id]/page.js
+++ app/(.)product/[id]/page.js
@@ -2,6 +2,16 @@
 import Modal from '@/components/Modal'
 import ProductDetails from '@/components/ProductDetails'
 
+
+export async function generateStaticParams() {
+    const res = await fetch('https://fakestoreapi.com/products');
+    const products = await res.json();
+
+    return products.map((product) => ({
+        id: String(product.id),
+    }));
+}
+
 async function page({ params: { id } }) {
     return (
         <Modal>
--- app/product/[id]/page.jsx
+++ app/product/[id]/page.jsx
@@ -1,8 +1,17 @@
 import ProductDetails from "@/components/ProductDetails";
 
-export default async function Product({params:{id}}) {
-  
+export async function generateStaticParams() {
+  const res = await fetch('https://fakestoreapi.com/products');
+  const products = await res.json();
+
+  return products.map((product) => ({
+    id: String(product.id),
+  }));
+}
+
+export default async function Product({ params: { id } }) {
+
   return (
-  <ProductDetails  id={id}/>
+    <ProductDetails id={id} />
   );
 }
